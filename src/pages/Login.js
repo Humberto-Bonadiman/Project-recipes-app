a1ae4e430ffe38da6accc2c7a4820062ab0f2ff6
Rodolfo Braga <rodolfo.braga@yahoo.com.br>
2021-11-05 16:05:50 -0300

    Requisitos 3 ao 5

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,22 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const isEmailValid = (userEmail) => {
+    const regexEmail = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/;
+    return regexEmail.test(userEmail);
+  };
+  const MIN_LENGTH = 7;
   return (
     <div>
       <input
         type="email"
         data-testid="email-input"
-        onChange={ () => {} }
+        onChange={ (e) => setEmail(e.target.value) }
       />
       <input
         type="password"
         data-testid="password-input"
-        onChange={ () => {} }
+        onChange={ (e) => setPassword(e.target.value) }
       />
       <button
         type="button"
         data-testid="login-submit-btn"
         onClick
+        disabled={ !(isEmailValid(email) && password.length >= MIN_LENGTH) }
       >
         Entrar
       </button>
