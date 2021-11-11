e091e92e816933403097e4857edcc004de6a1226
natalia <mellomartins.naty@gmail.com>
2021-11-11 15:55:55 -0300

    requisitos 41 e 42

--- src/components/ContinueRecipeButton.js
+++ src/components/ContinueRecipeButton.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import { useHistory } from 'react-router';
 import '../styles/StartRecipeButton.css';
 
 function ContinueRecipeButton() {
+  const history = useHistory();
+  const redirect = () => {
+    history.push(`${history.location.pathname}/in-progress`);
+  };
+
   return (
     <section>
       <button
         data-testid="start-recipe-btn"
         type="button"
         className="start-recipe-btn"
+        onClick={ redirect }
       >
         Continuar Receita
       </button>
--- src/components/StartRecipeButton.js
+++ src/components/StartRecipeButton.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import { useHistory } from 'react-router';
 import '../styles/StartRecipeButton.css';
 
 function StartRecipeButton() {
+  const history = useHistory();
+  console.log(history);
+  const redirect = () => {
+    history.push(`${history.location.pathname}/in-progress`);
+  };
+
   return (
     <section>
       <button
         data-testid="start-recipe-btn"
         type="button"
         className="start-recipe-btn"
+        onClick={ redirect }
       >
         Iniciar Receita
       </button>
