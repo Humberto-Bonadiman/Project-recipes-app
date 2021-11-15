7e01b7e45b6c42c1050380e3d5bb0a7bc900b7f4
Vitor Lima <vitorliman@gmail.com>
2021-11-15 17:35:02 -0300

    Finaliza requisitos 70 ao 73

--- src/pages/ExploreDrinks.js
+++ src/pages/ExploreDrinks.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import { useHistory } from 'react-router';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 function ExploreDrinks() {
+  const history = useHistory();
   return (
     <div>
       <Header title="Explorar Bebidas" />
+      <button
+        data-testid="explore-by-ingredient"
+        type="button"
+        onClick={ () => history.push('/explorar/bebidas/ingredientes') }
+      >
+        Por Ingredientes
+      </button>
+      <button
+        data-testid="explore-surprise"
+        type="button"
+      >
+        Me Surpreenda!
+      </button>
       <Footer />
     </div>
   );
--- src/pages/ExploreMeals.js
+++ src/pages/ExploreMeals.js
@@ -1,11 +1,33 @@
 import React from 'react';
+import { useHistory } from 'react-router';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 function ExploreMeals() {
+  const history = useHistory();
   return (
     <div>
       <Header title="Explorar Comidas" />
+      <button
+        data-testid="explore-by-ingredient"
+        type="button"
+        onClick={ () => history.push('/explorar/comidas/ingredientes') }
+      >
+        Por Ingredientes
+      </button>
+      <button
+        data-testid="explore-by-area"
+        type="button"
+        onClick={ () => history.push('/explorar/comidas/area') }
+      >
+        Por Local de Origem
+      </button>
+      <button
+        data-testid="explore-surprise"
+        type="button"
+      >
+        Me Surpreenda!
+      </button>
       <Footer />
     </div>
   );
