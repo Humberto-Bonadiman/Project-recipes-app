a9d340a54dc99ab8ee63541a258828e2b7a776c8
Rodolfo Braga <rodolfo.braga@yahoo.com.br>
2021-11-16 14:43:35 -0300

    Requisitos 82, 83 e 84

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -3,9 +3,29 @@ import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 function Profile() {
+  const { email } = JSON.parse(localStorage.getItem('user'));
   return (
     <div>
       <Header title="Perfil" />
+      <p data-testid="profile-email">{ email }</p>
+      <button
+        type="button"
+        data-testid="profile-done-btn"
+      >
+        Receitas Feitas
+      </button>
+      <button
+        type="button"
+        data-testid="profile-favorite-btn"
+      >
+        Receitas Favoritas
+      </button>
+      <button
+        type="button"
+        data-testid="profile-logout-btn"
+      >
+        Sair
+      </button>
       <Footer />
     </div>
   );
