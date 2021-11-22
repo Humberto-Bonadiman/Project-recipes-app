ef1aa230e4e78895e0ec4927a0139059a51962c6
Rodolfo Braga <rodolfo.braga@yahoo.com.br>
2021-11-22 00:16:58 -0300

    style: css login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import '../styles/Login.css';
+import appLogo from '../images/recipe-app-logo.png';
 
 function Login({ history }) {
   const [email, setEmail] = useState('');
@@ -20,31 +21,38 @@ function Login({ history }) {
 
   const MIN_LENGTH = 7;
   return (
-    <form className="login-form">
-      <input
-        type="email"
-        data-testid="email-input"
-        placeholder="Email"
-        className="email-input"
-        onChange={ (e) => setEmail(e.target.value) }
+    <main>
+      <img
+        src={ appLogo }
+        alt="Logotipo"
+        className="logo"
       />
-      <input
-        type="password"
-        data-testid="password-input"
-        placeholder="Senha"
-        className="password-input"
-        onChange={ (e) => setPassword(e.target.value) }
-      />
-      <button
-        type="button"
-        data-testid="login-submit-btn"
-        className="login-submit-btn"
-        onClick={ handleClick }
-        disabled={ !(isEmailValid(email) && password.length >= MIN_LENGTH) }
-      >
-        Entrar
-      </button>
-    </form>
+      <form className="login-form">
+        <input
+          type="email"
+          data-testid="email-input"
+          placeholder="Email"
+          className="email-input"
+          onChange={ (e) => setEmail(e.target.value) }
+        />
+        <input
+          type="password"
+          data-testid="password-input"
+          placeholder="Senha"
+          className="password-input"
+          onChange={ (e) => setPassword(e.target.value) }
+        />
+        <button
+          type="button"
+          data-testid="login-submit-btn"
+          className="login-submit-btn"
+          onClick={ handleClick }
+          disabled={ !(isEmailValid(email) && password.length >= MIN_LENGTH) }
+        >
+          Entrar
+        </button>
+      </form>
+    </main>
   );
 }
 
