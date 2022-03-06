ceb1fda1bd319e7ac091a9175f42682201416df1
Humberto-Bonadiman <humberto_bonadiman@hotmail.com>
2022-03-05 21:18:30 -0400

    Updates

---
+++ precache-manifest.38205ca94e31a66717b329d23ca9942f.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63f05c006903c1cc0542553972159473",
+    "url": "/Project-recipes-app/index.html"
+  },
+  {
+    "revision": "39fded6781865be47a2d",
+    "url": "/Project-recipes-app/static/css/2.95258f03.chunk.css"
+  },
+  {
+    "revision": "8f8ca52bb07b2ea2fd81",
+    "url": "/Project-recipes-app/static/css/main.33397ea6.chunk.css"
+  },
+  {
+    "revision": "39fded6781865be47a2d",
+    "url": "/Project-recipes-app/static/js/2.246889f9.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/Project-recipes-app/static/js/2.246889f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f8ca52bb07b2ea2fd81",
+    "url": "/Project-recipes-app/static/js/main.b618f9a7.chunk.js"
+  },
+  {
+    "revision": "b6a0a297aadcc5537cee",
+    "url": "/Project-recipes-app/static/js/runtime-main.f6c062d5.js"
+  },
+  {
+    "revision": "d0456e61764b31daadd9c88484cd79e4",
+    "url": "/Project-recipes-app/static/media/blackHeartIcon.d0456e61.svg"
+  },
+  {
+    "revision": "f87c47c37c0c0530fe7307af260caa25",
+    "url": "/Project-recipes-app/static/media/drinkIcon.f87c47c3.svg"
+  },
+  {
+    "revision": "157945fffa619b919f7bb65fe11b7b43",
+    "url": "/Project-recipes-app/static/media/exploreIcon.157945ff.svg"
+  },
+  {
+    "revision": "b27ba19ea033355b73926443af1756e9",
+    "url": "/Project-recipes-app/static/media/mealIcon.b27ba19e.svg"
+  },
+  {
+    "revision": "74e487e504d5cf16d7832bef2fbacfea",
+    "url": "/Project-recipes-app/static/media/profileIcon.74e487e5.svg"
+  },
+  {
+    "revision": "5b8a415e880c2e2dd08808214f79aed9",
+    "url": "/Project-recipes-app/static/media/recipe-app-logo.5b8a415e.png"
+  },
+  {
+    "revision": "db81213596cb3beb56440bb39aa70c8c",
+    "url": "/Project-recipes-app/static/media/searchIcon.db812135.svg"
+  },
+  {
+    "revision": "06215ed4cae23455c02abc804445ba74",
+    "url": "/Project-recipes-app/static/media/shareIcon.06215ed4.svg"
+  },
+  {
+    "revision": "ea3b6ba893ba3908dd84f6170b75545e",
+    "url": "/Project-recipes-app/static/media/whiteHeartIcon.ea3b6ba8.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/project-recipes-app/precache-manifest.bc6c965860dba568c8ca7c84b343cc94.js"
+  "/Project-recipes-app/precache-manifest.38205ca94e31a66717b329d23ca9942f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/project-recipes-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Project-recipes-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
